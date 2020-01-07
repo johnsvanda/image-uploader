425ff9701506d77428480a05c1735136f8a42d52
Jan Švanda <john.svanda@gmail.com>
2020-01-07 19:37:13 +0100

    drag area fixed

---
+++ build/precache-manifest.362ce9a4e4679053ae3382eebc3576c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49180a178760fc48af5e67e8d310f565",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7057439d01ae028ae10a",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c699a3a914d0c0611591",
+    "url": "/static/css/main.777a284a.chunk.css"
+  },
+  {
+    "revision": "7057439d01ae028ae10a",
+    "url": "/static/js/2.30e1d68d.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/static/js/2.30e1d68d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c699a3a914d0c0611591",
+    "url": "/static/js/main.647e26a3.chunk.js"
+  },
+  {
+    "revision": "562500fb81d748c47b0f",
+    "url": "/static/js/runtime-main.eac14714.js"
+  }
+]);
